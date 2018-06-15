6766f9ad112b5c93e124888ab8f5fd65c7513007
nguyenuyen <35299245+nguyenuyen@users.noreply.github.com>
2018-06-15 14:42:19 +0700

    Initial commit

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.*;
+import java.io.IOException;
+
+public class TestServlet implements Servlet{
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+
+    }
+}
